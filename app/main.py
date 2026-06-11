994c9fbbce98b9c878960bcdb64d012abf2a1649
Dilip Goud <dilipgaurh2017@gmail.com>
2026-06-11 12:01:19 +0530

    feat: add Swagger UI documentation endpoint and static assets

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.openapi.docs import get_swagger_ui_html
 from app.api.v1.routers import router as api_router
 from app.core.exceptions.exceptions import AppException
 from app.core.exceptions.exception_handlers import app_exception_handler
@@ -13,7 +16,28 @@ app = FastAPI(
     version="1.0.0",
     swagger_ui_parameters={"defaultModelsExpandDepth": -1},
     debug=os.getenv("DEBUG", False),
+    docs_url=None,
 )
+
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui_html(req: Request) -> HTMLResponse:
+    root_path = req.scope.get("root_path", "").rstrip("/")
+    openapi_url = root_path + app.openapi_url
+    oauth2_redirect_url = app.swagger_ui_oauth2_redirect_url
+    if oauth2_redirect_url:
+        oauth2_redirect_url = root_path + oauth2_redirect_url
+    return get_swagger_ui_html(
+        openapi_url=openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=oauth2_redirect_url,
+        init_oauth=app.swagger_ui_init_oauth,
+        swagger_favicon_url=f"{root_path}/static/expenseflow.svg",
+        swagger_ui_parameters=app.swagger_ui_parameters,
+    )
 
 
 app.add_exception_handler(
